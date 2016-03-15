abc2e3fbd0daad9ceb17e4771d2946a3553c5a7b
zhangguangxi <zhangguangxito@sina.cn>
2016-03-16 00:27:57 +0800

    modify 2016-03-15

--- logger/src/main/java/com/zgx/logback/LogbackDemo.java
+++ logger/src/main/java/com/zgx/logback/LogbackDemo.java
@@ -18,10 +18,12 @@ public class LogbackDemo {
     private static Logger log = LoggerFactory.getLogger(LogbackDemo.class);
     
     public static void main(String[] args) {
+        log.info("main>>>>>>开始");
         log.trace("=====trace");
         log.debug("=====debug");
         log.info("=====info");
         log.warn("=====warn");
         log.error("=====error");
+        log.info("main>>>>>>结束");
     }
 }
